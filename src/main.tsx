1e174945c6b2cdfc49a48110f244990b5235cb25
Иван Синев <ie.sinev.work@gmail.com>
2026-06-02 09:45:58 +0300

    error 404 fix

--- src/main.tsx
+++ src/main.tsx
@@ -6,16 +6,16 @@ import './index.css'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store = {store}>
       <MantineProvider>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </MantineProvider>
     </Provider>
   </StrictMode>,
